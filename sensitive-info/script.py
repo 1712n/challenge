586082158559f7668ab6a10af950695d08987fc9
Abduaziz <abduaziz.ziyodov@mail.ru>
2023-05-20 13:20:31 +0500

    Implement report generator, and main function

--- sensitive-info/script.py
+++ sensitive-info/script.py
@@ -1,22 +1,16 @@
-import os
+import re
 import httpx
-import dotenv
 import typing as t
 from rich import print
 
 from regexs import REGEXS
-
-
-dotenv.load_dotenv()
-
-
-GITHUB_API_URL: str = "https://api.github.com"
-
-HEADERS: dict = {
-    "X-GitHub-Api-Version": "2022-11-28",
-    "Accept": "application/vnd.github+json",
-    "Authorization": "Bearer {}".format(os.getenv("GITHUB_TOKEN")),
-}
+from constants import (
+    GITHUB_API_URL,
+    HEADERS,
+    TEST_REPO_OWNER,
+    TEST_REPO,
+    REPORT_SAVE_PATH,
+)
 
 
 class Github:
@@ -40,6 +34,11 @@ class Github:
 
     # Issue actions
     def get_issues(self) -> dict:
+        """Fetch list of all issues. Somehow, github rest API
+        includes pull requests also. I mean, this function returns
+        issues + pull requests. We don't need to write another function
+        for pull requests.
+        """
         return self.send_request("GET", f"{self.repository_url}/issues")
 
     def get_issue(self, number: int) -> dict:
@@ -50,29 +49,75 @@ class Github:
             "GET", f"{self.repository_url}/issues/{number}/comments"
         )
 
-    # Pull request actions
-
 
 def check_content(resource: dict, content: str) -> t.Generator[t.Dict, t.Any, t.Any]:
     """Generator function that compares content(string) with all available
-    regex patterns. Returns resulkt
+    regex patterns.
     """
-    results: t.List[t.Dict] = list()
-
     for regex in REGEXS:
-        for line in content.split("\n"):
-            if value := regex["regex"].search(line):
+        for line in re.split(r"\n|,|!|;", content):
+            if value := regex.get("pattern").search(line):
                 yield {
                     "value": value.group(),
                     "type": regex["type"],
                     "url": resource.get("html_url"),
-                    "line": line.__str__(),
+                    "part": line.__str__(),
                 }
 
 
-def create_report(results: t.List[t.Dict]) -> str:
-    ...
+def report_generator(results: t.List[t.Dict]) -> t.Generator[str, t.Any, t.Any]:
+    # Terminal text - with colored output :D
+    report: str = "[bold red]\n[ Sensitive Informations ][/ bold red]\n"
+
+    for result in results:
+        report += (
+            f"[bold medium_purple1]Type[/ bold medium_purple1]: {result['type']}\n"
+        )
+        report += f"[bold medium_purple1]Info value[/ bold medium_purple1]: {result['value']}\n"
+        report += f"[bold medium_purple1]Part of the information[/ bold medium_purple1]: {result['part']}\n"
+        report += f"[bold medium_purple1]Link(source)[/ bold medium_purple1]: {result['url']}\n\n"
+
+    yield report
+
+    colors: str = [
+        "[bold red]",
+        "[/ bold red]",
+        "[bold medium_purple1]",
+        "[/ bold medium_purple1]",
+    ]
+
+    for piece_to_remove in colors:
+        report = report.replace(piece_to_remove, "")
+
+    yield report
+
+
+def main() -> None:
+    """Main function for our script.
+    Performs search, reports about results (+save).
+    """
+    github = Github(TEST_REPO_OWNER, TEST_REPO)
+
+    issue_results: t.List[dict] = [
+        result
+        for issue in github.get_issues()
+        for result in check_content(issue, issue["body"])
+    ]
+
+    comment_results: t.List[dict] = [
+        result
+        for issue in github.get_issues()
+        for comment in github.get_issue_comments(issue["number"])
+        for result in check_content(issue, comment["body"])
+    ]
+
+    generator = report_generator(issue_results + comment_results)
+
+    print(next(generator))  # first time it yields terminal output
+
+    with open(REPORT_SAVE_PATH, "w") as file:
+        file.write(next(generator))  # then raw text
 
 
 if __name__ == "__main__":
-    github = Github("abduazizziyodov", "test-repo")
+    main()
